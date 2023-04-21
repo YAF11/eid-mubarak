55de4e1e32746f51f2de57915fec5ebbee414421
Yafi Irsyad <107847360+YAF11@users.noreply.github.com>
2023-04-21 17:16:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Happy Eid Mubarak 1443 H
+
+Selamat Idul Fitri 1443 Hijriah, semoga Allah senantiasa melimpahkan rahmatnya kepada kita semua dan segala amal perbuatan selama bulan ramadhan kita dapat diterima
+
+## Deployment
+
+Terbit di halaman [GitHub](https://chrisnaadhi.github.io/eid-mubarak/)
+
+## Installation
+
+Install project ini dengan melakukan clone atau degit
+
+```bash
+  npx degit https://github.com/chrisnaadhi/eid-mubarak
+  npm install
+  npm run dev
+```
+# eid-mubarak
---
+++ src/App.svelte
@@ -0,0 +1,138 @@
+<script>
+  import JSConfetti from "js-confetti";
+  import { onMount } from "svelte";
+
+  const confetti = new JSConfetti();
+  const date = new Date();
+  let year = "1444";
+  let shake = false;
+  let maaf = false;
+
+  function showConfetti() {
+    confetti.addConfetti();
+    maaf = true;
+    shake = true;
+    setTimeout(() => {
+      shake = false;
+      maaf = false;
+    }, 1000);
+  }
+
+  function intro() {
+    confetti.addConfetti();
+  }
+
+  onMount(() => {
+    intro();
+  });
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  <script
+    src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"
+  ></script>
+  <title>
+    Happy Eid Mubarak {year}H | Yafi' Irsyad Ramadhan
+  </title>
+</svelte:head>
+
+<main>
+  <div class="animate__animated animate__fadeIn">
+    <h1>Eid Mubarak!</h1>
+    <h2>{year} Hijriah</h2>
+    <p>Selamat Idul Fitri {year} H</p>
+    <p class={maaf ? "maaf animate__animated animate__heartBeat" : "maaf"}>
+      Taqabbalallahu Minna Wa Minkum
+    </p>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <span
+      on:click={showConfetti}
+      class={shake ? "salam animate__animated animate__shakeY" : "salam"}
+      >🤝</span
+    >
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <p on:click={showConfetti} class="salam-teks">Klik untuk bersalaman</p>
+  </div>
+  <footer>
+    <p>
+      {date.getFullYear()} | Yafi' Irsyad Ramadhan
+    </p>
+  </footer>
+</main>
+
+<style>
+  :global(*) {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  main {
+    height: 100%;
+    background-image: url("bg.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    position: relative;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 20em;
+  }
+
+  .wonder {
+    font-size: 3em;
+  }
+
+  h1,
+  h2 {
+    color: #fff;
+  }
+
+  p {
+    text-align: center;
+    color: #fff;
+  }
+
+  .maaf {
+    font-size: 1.2em;
+  }
+
+  .salam {
+    font-size: 4rem;
+    position: absolute;
+    margin-top: 12rem;
+    cursor: pointer;
+  }
+
+  .salam-teks {
+    position: absolute;
+    margin-top: 17rem;
+    font-style: italic;
+    cursor: pointer;
+  }
+
+  footer {
+    color: #fff;
+    font-style: italic;
+    bottom: 0;
+    position: absolute;
+    width: 100%;
+  }
+
+  footer > p {
+    font-size: 1em;
+    padding-bottom: 1em;
+  }
+
+  a {
+    color: orangered;
+  }
+</style>
